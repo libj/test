7dd92aeee55ee94cb481cf178bd314c3ca3eb05b
Seva Safris <seva@safris.org>
2019-05-04 15:30:14 +0200

    Rebrand as LibJ

--- src/test/java/org/libj/test/SimpleNamespaceContextTest.java
+++ src/test/java/org/libj/test/SimpleNamespaceContextTest.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014 OpenJAX
+/* Copyright (c) 2014 LibJ
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -14,7 +14,7 @@
  * program. If not, see <http://opensource.org/licenses/MIT/>.
  */
 
-package org.openjax.test;
+package org.libj.test;
 
 import static org.junit.Assert.*;
 
